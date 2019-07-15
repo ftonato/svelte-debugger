869d3d72a1d47241bdcc4a360089268f088fc954
Ademílson F. Tonato <ademilsonft@outlook.com>
2019-07-15 13:10:32 +0100

    Create a Svete component to help us to debug

---
+++ Debugger.svelte
@@ -0,0 +1,75 @@
+<script>
+  import formatHighlight from "json-format-highlight";
+  import { onMount } from "svelte";
+
+  export let data = {};
+  export let indent = 2; // spaces
+
+  // formatHighlight - Define your exclusive color scheme
+  export let colorOptions = {
+    keyColor: "dimgray",
+    numberColor: "lightskyblue",
+    stringColor: "lightcoral",
+    trueColor: "lightseagreen",
+    falseColor: "#f66578",
+    nullColor: "cornflowerblue"
+  };
+
+  let isPropsValid = false;
+
+  onMount(() => {
+    init();
+  });
+
+  const validateProp = prop => {
+    if (typeof prop === "object") {
+      return !!Object.keys(prop).length;
+    }
+
+    if (typeof prop === "boolean") {
+      return prop;
+    }
+
+    if (typeof prop === "number") {
+      return prop > 0;
+    }
+  };
+
+  const init = () => {
+    isPropsValid = validateProp(data) && validateProp(indent);
+  };
+
+  const getFormatted = () => {
+    return formatHighlight(JSON.stringify(data, null, indent), colorOptions);
+  };
+</script>
+
+<style>
+  pre {
+    position: fixed;
+    bottom: 15px;
+    left: 1%;
+
+    box-sizing: border-box;
+
+    width: 98%;
+    min-height: 150px;
+    max-height: 250px;
+    overflow-y: auto;
+
+    border: 2px solid #131313;
+    background-color: #efefef;
+  }
+  pre code {
+    font-size: 12px;
+    line-height: 20px;
+  }
+</style>
+
+{#if isPropsValid}
+  <pre>
+    <code>
+      {@html getFormatted()}
+    </code>
+  </pre>
+{/if}
---
+++ README.md
@@ -0,0 +1,55 @@
+# svelte-debugger
+
+[![npm][badge-version]][npm]
+[![bundle size][badge-size]][bundlephobia]
+[![license][badge-license]][license]
+
+A debugger component for [Svelte][svelte] apps.
+
+## Installation
+
+```bash
+npm install svelte-debugger --save
+```
+
+or
+
+```bash
+yarn add svelte-debugger
+```
+
+## Usage
+
+```html
+<script>
+  import Debugger from 'svelte-debugger';
+</script>
+
+<Debugger data={example} />
+```
+
+## Properties
+
+```html
+<Debugger
+  data={example}
+  indent={2}
+  colorOptions={{ falseColor: '#ff3e00', trueColor: '#40b3ff' }}
+/>
+```
+
+- `data`: object to debug
+- `indent`: `number` size of spaces between _keys_ and _values_
+- `colorOptions`: `object` containing at least one of the properties `keyColor`, `numberColor`, `stringColor`, `trueColor`, `falseColor`, `nullColor`.
+
+[svelte]: https://svelte.dev/
+[badge-version]: https://img.shields.io/npm/v/svelte-debugger.svg
+[npm]: https://www.npmjs.com/package/svelte-debugger
+[badge-size]: https://img.shields.io/bundlephobia/minzip/svelte-debugger.svg
+[bundlephobia]: https://bundlephobia.com/result?p=svelte-debugger
+[badge-license]: https://img.shields.io/npm/l/svelte-debugger.svg
+[license]: https://github.com/ftonato/svelte-debugger/blob/master/LICENSE
+
+## License
+
+[MIT](LICENSE)
